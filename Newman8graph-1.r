15459de24b72b439f01c93864b375178b86cb116
ggosti <ggosti@uci.edu>
2012-08-08 11:08:39 -0700

    fixed bugs

--- Newman8graph-1.r
+++ Newman8graph-1.r
@@ -12,44 +12,52 @@ t1<-matrix(nrow=8,ncol=8,seq(0,0,length=64))
  t1[8,1]<-1
  t2<-t1+t(t1)
  toy<-graph.adjacency(t2)
- vertex.disjoint.paths(toy,0,3) # nips test
- ALL EFFORTS BELOW HAVE THIS ERROR
- Error in vertex.disjoint.paths(toy, i, j) : 
-  At flow.c:2086 : The source==target case is not implemented, Unimplemented function call #DUE TO adjacent pairs *should = 1)
-#TOLGA'S TRY
-a<-rep(0,64)
-a<-matrix(a,nrow=8,ncol=8)
-for(i in 1:8){
- for (j in 1:8){
-   if (a[i,j]==1) a[i,j]<-1 else # Fix this line
-   #or vectorized ifelse
-	a[i,j]<-vertex.disjoint.paths(toy,i-1,j-1) #end else
-}
-}
-#TOLGA'S TRY 2
-a<-rep(0,64)
-a<-matrix(a,nrow=8,ncol=8,seq(0,0,length=64))
-for(i in 0:7){
- for (j in 0:7){
+ vertex.disjoint.paths(toy,1,3) # nips test
+a<-matrix(nrow=8,ncol=8,seq(0,0,length=64))
+for(i in 1:(length(V(toy)) - 1)){
+ for (j in (i+1):length(V(toy))){
+#	print(i)
+#	print(j)
 	a[i,j]<-vertex.disjoint.paths(toy,i,j)
 }
 }
- #DOUG'S TRY
- t3<-rep(0,64)
- t3<-matrix(nrow=8,ncol=8,seq(0,0,length=64))
- for (i in 0:7) {
-     (j in 0:7) {t3<-vertex.disjoint.paths(toy,i,j)
-                 }} # nips 
+a<-a+t(a)
 
-NO NEED TO VECTORIZE
-     [,1] [,2] [,3] [,4] [,5] [,6] [,7] [,8]
-[1,]    0    1    0    0    0    0    0    1
-[2,]    1    0    1    0    0    0    0    0
-[3,]    0    1    0    1    0    0    0    0
-[4,]    0    0    1    0    1    0    0    0
-[5,]    0    0    0    1    0    1    0    0
-[6,]    0    0    0    0    1    0    1    0
-[7,]    0    0    0    0    0    1    0    1
-[8,]    1    0    0    0    0    0    1    0
+# ALL EFFORTS BELOW HAVE THIS ERROR
+# Error in vertex.disjoint.paths(toy, i, j) : 
+#  At flow.c:2086 : The source==target case is not implemented, Unimplemented function call
+#TOLGA'S TRY
+#a<-rep(0,64)
+#a<-matrix(a,nrow=8,ncol=8)
+#for(i in 1:8){
+# for (j in 1:8){
+#	a[i,j]<-vertex.disjoint.paths(toy,i-1,j-1)
+#}
+#}
+#TOLGA'S TRY 2
+#a<-rep(0,64)
+#a<-matrix(a,nrow=8,ncol=8,seq(0,0,length=64))
+#for(i in 0:7){
+# for (j in 0:7){
+#	a[i,j]<-vertex.disjoint.paths(toy,i,j)
+#}
+#}
+# #DOUG'S TRY
+# t3<-rep(0,64)
+# t3<-matrix(nrow=8,ncol=8,seq(0,0,length=64))
+# for (i in 0:7) {
+#     (j in 0:7) {t3<-vertex.disjoint.paths(toy,i,j)
+#                 }} # nips 
+
+#NO NEED TO VECTORIZE
+#     [,1] [,2] [,3] [,4] [,5] [,6] [,7] [,8]
+#[1,]    0    1    0    0    0    0    0    1
+#[2,]    1    0    1    0    0    0    0    0
+#[3,]    0    1    0    1    0    0    0    0
+#[4,]    0    0    1    0    1    0    0    0
+#[5,]    0    0    0    1    0    1    0    0
+#[6,]    0    0    0    0    1    0    1    0
+#[7,]    0    0    0    0    0    1    0    1
+#[8,]    1    0    0    0    0    0    1    0
 
 
