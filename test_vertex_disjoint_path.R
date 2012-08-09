02d126f91324410c9edb55148d53f11d8fab07b5
Steve Doubleday <stevedoubleday@gmail.com>
2012-08-08 22:40:47 -0700

    vertex disjoint paths now return list of count and vertices

--- test_vertex_disjoint_path.R
+++ test_vertex_disjoint_path.R
@@ -9,25 +9,26 @@ test_that("Disjoint paths calculated correctly",
 {
   g <- graph.adjacency(n1)   # n1 from sample_net.R 
   vertices <- c(1,2)
-  expect_that(3,equals(vertex_disjoint_paths(vertices,g)))
+  expect_that(3,equals(vertex_disjoint_paths(vertices,g)[[1]]))
   vertices <- c(3,2)  # should fail; target must be greater than source
   vertex_disjoint_paths(vertices,g)
-  expect_that(-99,equals(vertex_disjoint_paths(vertices,g)))
+  expect_that(-99,equals(vertex_disjoint_paths(vertices,g)[[1]]))
   vertices <- c(2,2)  # should fail; source can't equal target 
-  expect_that(-98,equals(vertex_disjoint_paths(vertices,g)))
+  expect_that(-98,equals(vertex_disjoint_paths(vertices,g)[[1]]))
   vertices <- c(0,2)  # should fail; 0 invalid
-  expect_that(-97,equals(vertex_disjoint_paths(vertices,g)))
+  expect_that(-97,equals(vertex_disjoint_paths(vertices,g)[[1]]))
   vertices <- c(1,10)  # should fail; target out of range
-  expect_that(-96,equals(vertex_disjoint_paths(vertices,g)))
+  expect_that(-96,equals(vertex_disjoint_paths(vertices,g)[[1]]))
   vertices <- c("a","b")  # should fail; must be numeric
-  expect_that(-95,equals(vertex_disjoint_paths(vertices,g)))
+  expect_that(-95,equals(vertex_disjoint_paths(vertices,g)[[1]]))
 })
 
 test_that("Disjoint paths calculated correctly",
 {
   g <- graph.adjacency(n1)   # n1 from sample_net.R 
   lv <- list(c(1,2), c(2,3), c(4,5), c(5,6), c(6,7))
-#  expect_that(3,equals(vertex_list_disjoint_paths(lv,g)))
+  path_list <- vertex_disjoint_path_list(lv,g)
+  expect_that(3,equals(path_list[[1]][[1]])) # disjoint path count...
+  expect_that(c(1,2),equals(path_list[[1]][[2]])) # for vertices(1,2)
 #  vertices <- c(3,2)  # should fail; target must be greater than source
-}
-  #out <- lapply(L, vertex_disjoint_paths, graph)
+})
--- vertex_disjoint_path.R
+++ vertex_disjoint_path.R
@@ -2,15 +2,26 @@
 
 require(igraph)
 
+build_list <- function(c, verts)
+{
+  list(count=c, vertices=verts)
+}
+
+
+
 vertex_disjoint_paths <- function(vertices, graph)
 {
   source <- vertices[1]
   target <- vertices[2]
   num_vertices <- length(V(graph))
-  if (mode(vertices) != "numeric") { return (-95) }  
-  if (target < source) {  return (-99) }
-  if (target == source) {  return (-98) }
-  if ((target <= 0) || (source <= 0)) { return (-97) }
-  if ((target > num_vertices) || (source > num_vertices)) { return (-96) }
-  vertex.disjoint.paths(graph, source, target)
+  if (mode(vertices) != "numeric") { return(build_list(-95,vertices)) }  
+  if (target < source) {  return(build_list(-99,vertices)) }
+  if (target == source) {  return(build_list(-98,vertices)) }
+  if ((target <= 0) || (source <= 0)) { return(build_list(-97,vertices)) }
+  if ((target > num_vertices) || (source > num_vertices)) { return(build_list(-96,vertices)) }
+  build_list(vertex.disjoint.paths(graph, source, target),vertices)
+}
+vertex_disjoint_path_list <- function(vertex_list, graph)
+{
+  lapply(vertex_list, vertex_disjoint_paths, graph)
 }
