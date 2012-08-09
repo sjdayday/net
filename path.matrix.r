10e870b19d335352818a724108ebb8d2a561ee88
Tolga Oztan <boztan@uci.edu>
2012-08-09 10:48:58 -0700

    path.matrix modified

--- path.matrix.r
+++ path.matrix.r
@@ -1,5 +1,5 @@
 path.matrix<-function(path.list,n){
-  pmatrix<-matrix(nrow=n,ncol=n)
+  pmatrix<-matrix(rep(0,n*n),nrow=n,ncol=n)
   for(i in 1:length(path.list)){
     pmrow<- unlist(path.list[[i]][2])[1]
     pmcol<- unlist(path.list[[i]][2])[2]
