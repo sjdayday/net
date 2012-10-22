a0d6ecd3e0c114f786921910633ec488c8748491
Steve Doubleday <stevedoubleday@gmail.com>
2012-10-22 14:00:21 -0700

    Prepare to run disjoint paths for NORD2 data

---
+++ vertex_disjoint_paths_NORD2.R
@@ -0,0 +1,36 @@
+require(igraph)
+require(multicore)
+
+source("vertex_disjoint_path.R")
+source("upTriangle.R")
+source("path.matrix.r")
+options(cores= 2)
+
+  print("cores: ")
+  print(getOption("cores"))
+  print("before creating network")
+  timestamp()
+#  set.seed(123456)
+#  g <- watts.strogatz.game(1, 300 , 10 , 0.05)
+  g <- read.graph(file="http://intersci.ss.uci.edu/wiki/pdf/0SDSC/NORD2.net", format="pajek")
+  numV = length(V(g))
+
+  print("before creating vertex pair list")
+  timestamp()
+  vertex_pair_list <- upTriangle(numV)
+
+  print("before starting mclapply")
+  timestamp()
+  path_list <- vertex_disjoint_path_list(vertex_pair_list,g)
+
+  print("before building final path matrix")
+  timestamp()
+  matrix <- path.matrix(path_list,numV)
+
+  #Note that this file is overwritten on subsequent runs
+  print("before writing the matrix file")
+  timestamp()
+  write.csv(matrix, "matrix_NORD2.csv")
+
+  print("end of job")
+  timestamp()
